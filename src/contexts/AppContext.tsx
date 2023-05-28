e2da6a4a6d57fd0687c23ff9004315e7465ae4b8
Victor Silva <victormarques.ia@gmail.com>
2023-05-28 01:29:57 -0300

    feat: create AppContext

---
+++ src/contexts/AppContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState, useEffect, ReactNode } from "react";
+
+interface AppContextProps {
+  token: string | null;
+  setToken: (token: string) => void;
+  removeToken: () => void;
+}
+
+const AppContext = createContext<AppContextProps>({
+  token: null,
+  setToken: () => {},
+  removeToken: () => {},
+});
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+function AppProvider({ children }: AppProviderProps) {
+  const [token, setToken] = useState<string | null>(null);
+
+  const setTokenInLocalStorage = (token: string) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+  };
+
+  const removeTokenFromLocalStorage = () => {
+    localStorage.removeItem("token");
+    setToken(null);
+  };
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  const contextValue: AppContextProps = {
+    token,
+    setToken: setTokenInLocalStorage,
+    removeToken: removeTokenFromLocalStorage,
+  };
+
+  return (
+    <AppContext.Provider value={contextValue}>{children}</AppContext.Provider>
+  );
+}
+
+export { AppContext, AppProvider };
