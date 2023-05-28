828391eef78c98b6f406a7e272e22a892bb10af5
Victor Silva <victormarques.ia@gmail.com>
2023-05-28 16:20:52 -0300

    feat: create postFormSchema

---
+++ src/utils/schemas/postFormSchema.ts
@@ -0,0 +1,14 @@
+import * as z from "zod";
+
+const postFormSchema = z.object({
+  title: z
+    .string()
+    .min(5, { message: "Título deve ter no mínimo 5 caracteres" }),
+  description: z
+    .string()
+    .min(10, { message: "Conteúdo deve ter no mínimo 10 caracteres" }),
+});
+
+export type PostFormType = z.infer<typeof postFormSchema>;
+
+export default postFormSchema;
